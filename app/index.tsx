b3f01fe972b78698bb8c1e0224be171f650a01f0
Jaden Snyder <jaysnyderrr@gmail.com>
2025-12-05 09:44:24 -0500

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-greenc-500 font-bold">SHREDDED</Text>
+    </View>
+  );
+}
