7bbd978cc2a5da6c5ea8394f5ac0f61d693123c2
Jaden Snyder <jaysnyderrr@gmail.com>
2025-12-07 13:20:49 -0500

    Fixed unmatched route error and navigation, by moving/renaming files and creating another folder (stack) within the app folder. Navigation beween screens --> sub-screens with tabs and links works great for now.

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -1,5 +1,3 @@
-// app/(stack)/_layout.tsx
-
 import { Stack } from "expo-router";
 
 export default function StackLayout() {
@@ -9,16 +7,10 @@ export default function StackLayout() {
       <Stack.Screen
         name="changePlan"
         options={{
-          // The title to show in the header when this screen is active
           title: "Change Plan",
-          // You can customize the header here, e.g., to show a back button
           headerShown: true,
         }}
       />
-
-      {/* add <Stack.Screen> definitions for any other 
-        non-tab screen you move into the (stack) folder here.
-      */}
     </Stack>
   );
 }
